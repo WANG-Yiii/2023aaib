a6704df1939e0327e1f884bb579bf2c1714987d2
WANG-Yiii <naroragrace@gmail.com>
2023-12-15 11:52:55 +0800

    week14

---
+++ week14/week14-6a.py
@@ -0,0 +1,12 @@
+#SOIT_BASE_009
+a = int(input())
+
+if a-2 > 0:
+	print( a - 2, end='' )
+else:
+	print( 2 - a, end='' )	
+	
+#SOIT_BASE_009
+a = int(input())
+
+print( abs(a-2), end='' )
---
+++ week14/week14-6b.py
@@ -0,0 +1,14 @@
+#SOIT_BASE_010
+# 31 28 31 30 31 30 31 31 30 31 30 31
+# Big   Big   Big   BigBig   Big   Big
+#    28    Small Small    Small Small
+a = int(input())
+if a==2: print(28, end='')
+elif a==4 or a==6 or a==9 or a==11: print(30, end='')
+else: print(31, end='')
+
+#SOIT_BASE_010
+a = int(input())
+
+ans = [0, 31,28,31,30,31,30,31, 31,30,31,30,31]
+print( ans[a], end='' )
