68e68db25d0a2878a96cacf6ca9156b6ee29ce6a
WANG-Yiii <naroragrace@gmail.com>
2024-01-05 11:53:46 +0800

    week17

---
+++ week17/week17-5a.py
@@ -0,0 +1,7 @@
+#SOIT108_BASE_010
+a, b = list(map(int, input().split() ))
+
+#print( a/b )
+ans = a//b
+if a%b>0: ans += 1
+print( ans, end='' )
---
+++ week17/week17-5d.py
@@ -0,0 +1,5 @@
+#SOIT108_BASE_013A
+a = list(map(int, input().split() ))
+
+#print( min(a), max(a) )
+print( f'[{min(a)},{max(a)}]', end='')
